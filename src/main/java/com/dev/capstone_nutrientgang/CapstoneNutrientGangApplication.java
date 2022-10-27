9e71a1407d04d5ce53781eeeba79788f3daa9ce7
hongjinwon <herenever@naver.com>
2022-10-28 02:02:53 +0900

    feat(프로젝트 생성): 프로젝트 생성 commit

---
+++ src/main/java/com/dev/capstone_nutrientgang/CapstoneNutrientGangApplication.java
@@ -0,0 +1,13 @@
+package com.dev.capstone_nutrientgang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneNutrientGangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneNutrientGangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/capstone_nutrientgang/CapstoneNutrientGangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.capstone_nutrientgang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneNutrientGangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
