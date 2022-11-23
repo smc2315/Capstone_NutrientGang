17b11e699c73f81cee1e22f71344dec79460d9d3
hongjinwon <herenever@naver.com>
2022-11-24 04:19:37 +0900

    refactor(utils): AttributeConvertor Null 타입 변경 수정

--- src/main/java/com/dev/utils/StringArrayConverter.java
+++ src/main/java/com/dev/utils/StringArrayConverter.java
@@ -2,8 +2,10 @@ package com.dev.utils;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Converter
@@ -11,11 +13,17 @@ public class StringArrayConverter implements AttributeConverter<List<String>,Str
 
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
+        if (Objects.isNull(attribute)){
+            return "";
+        }
         return String.join(",", attribute);
     }
 
     @Override
     public List<String> convertToEntityAttribute(String dbData) {
+        if (Objects.isNull(dbData)){
+            return new ArrayList<>();
+        }
         return Arrays.stream(dbData.split(","))
                 .collect(Collectors.toList());
     }
