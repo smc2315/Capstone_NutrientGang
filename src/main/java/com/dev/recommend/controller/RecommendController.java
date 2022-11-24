93a01edbd40be1e9a45c90ec131a94a658e1d3ea
최용태 <smc2315@naver.com>
2022-11-24 15:44:12 +0900

    feat: recommend 생성

---
+++ src/main/java/com/dev/recommend/controller/RecommendController.java
@@ -0,0 +1,4 @@
+package com.dev.recommend.controller;
+
+public class RecommendController {
+}
---
+++ src/main/java/com/dev/recommend/service/RecommendService.java
@@ -0,0 +1,69 @@
+package com.dev.recommend.service;
+
+
+import com.dev.recommend.dto.NutrientDto;
+import com.dev.recommend.dto.NutrientStatusInfoResDto;
+import com.dev.recommend.dto.CalorieInfoResDto;
+import com.dev.recommend.repository.HealthStatusRepository;
+import com.dev.recommend.repository.NutrientStatusRepository;
+import com.dev.jwt.utils.SecurityUtil;
+import com.dev.utils.response.BaseException;
+import com.dev.utils.response.BaseResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendService {
+    private final HealthStatusRepository healthStatusRepository;
+    private final NutrientStatusRepository nutrientStatusRepository;
+
+    @Transactional
+    public CalorieInfoResDto getCalorieInfo(LocalDate date){
+        Long userId = SecurityUtil.getCurrentMemberId();
+        Integer needCalorie = healthStatusRepository.findNeedCalorieByMemberAndDate(userId, date);
+        if (needCalorie == null) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_HEALTH_STATUS);
+        }
+
+        Integer haveCalorie = nutrientStatusRepository.findHaveCalorieByMemberAndDate(userId, date);
+        if (haveCalorie == null) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_NUTRIENT_STATUS);
+        }
+
+        return CalorieInfoResDto.builder()
+                .needCalorie(needCalorie)
+                .haveCalorie(haveCalorie)
+                .build();
+    }
+
+    @Transactional
+    public NutrientStatusInfoResDto getNutrientInfo(LocalDate date){
+        Long userId = SecurityUtil.getCurrentMemberId();
+
+        // 섭취해야할 영양소 계산
+        Optional<NutrientDto> needNutrientInfo = healthStatusRepository.findNeedNutrientByMemberAndDate(userId, date);
+        if(needNutrientInfo.isEmpty()){
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_HEALTH_STATUS);
+        }
+        NutrientDto needNutrient = needNutrientInfo.get();
+
+
+        // 섭취한 영양소 값
+        Optional<NutrientDto> haveNutrientInfo = nutrientStatusRepository.findHaveNutrientInfoByMemberAndDate(userId, date);
+        if(haveNutrientInfo.isEmpty()){
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_NUTRIENT_STATUS);
+        }
+        NutrientDto haveNutrient = haveNutrientInfo.get();
+
+        return NutrientStatusInfoResDto.builder()
+                .needNutrient(needNutrient)
+                .haveNutrient(haveNutrient)
+                .build();
+
+    }
+}
