dc5a3605b408e76fee720c6b0def396adc2056b5
최용태 <smc2315@naver.com>
2022-11-25 20:29:18 +0900

    feat: 아침 추천

--- src/main/java/com/dev/register/repository/FoodRepository.java
+++ src/main/java/com/dev/register/repository/FoodRepository.java
@@ -1,12 +1,14 @@
 package com.dev.register.repository;
 
+import com.dev.register.entity.Breakfast;
 import com.dev.register.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food,Long> {
     Optional<Food> findByName(String name);
 
-
+    Optional<List<Food>> findByCalorieBetween(Double minCalorie, Double maxCalorie);
 }
